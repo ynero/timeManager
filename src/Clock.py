71a65de8dc1c58a9e74d1d00917fe387d7a26fa4
Nero <oren.yair1@gmail.com>
2017-10-07 23:18:00 -0700

    Adding spaces

--- src/Clock.py
+++ src/Clock.py
@@ -16,18 +16,23 @@ class CTime:
 		self.hh = hh
 		self.mm = mm
 		self.ss = ss
+		
 	def GetHH24(self):
 		return self.hh
+		
 	def GetHH12(self):
 		if(self.hh > 12):
 			return self.hh-12
 		elif(self.hh ==0):
 			return 12;
 		return self.hh
+		
 	def GetMM(self):
 		return self.mm
+		
 	def GetSS(self):
 		return self.ss
+		
 	def GetAmPm(self):
 		if(self.hh > 12):
 			return "PM"
@@ -37,6 +42,7 @@ class Clock:
 	def GetCurrTime(self):
 		self.lastTime = datetime.datetime.now()
 		return CTime(self.lastTime.hour, self.lastTime.minute, self.lastTime.second)
+		
 	def GetWeekDay(self):
 		return WeekDays(self.lastTime.weekday())
 
